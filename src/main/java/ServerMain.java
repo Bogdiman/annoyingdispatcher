e2f11e79cc4d32a5aa94f022322ac79f87df116c
Bogdan <bogdan.bitfoi@gmail.com>
2017-05-21 18:04:14 +0300

    No more import wildcards

--- src/main/java/ServerMain.java
+++ src/main/java/ServerMain.java
@@ -1,7 +1,9 @@
 import com.sun.management.OperatingSystemMXBean;
 import org.apache.log4j.Logger;
 
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.net.ServerSocket;
 import java.net.Socket;
